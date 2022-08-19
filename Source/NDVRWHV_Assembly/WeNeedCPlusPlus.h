34a36f58c054adc5e30a852878fd44840f1824fe
NDmicha <56629459+NDmicha@users.noreply.github.com>
2022-08-19 09:07:38 +0200

    Converted to C++ project

---
+++ Source/NDVRWHV_Assembly/WeNeedCPlusPlus.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class NDVRWHV_ASSEMBLY_API WeNeedCPlusPlus
+{
+public:
+	WeNeedCPlusPlus();
+	~WeNeedCPlusPlus();
+};
